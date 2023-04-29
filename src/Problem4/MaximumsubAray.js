ff436f45091817def6dd4d7d877deda8c091a7d2
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-04-29 15:43:25 +0530

    fibonacci

---
+++ src/Problem4/MaximumsubAray.js
@@ -0,0 +1,11 @@
+let arr = [1, -2, -3, 4, -2, 6, -2, -2];
+
+// for (let i = 0; i < arr.length; i++) {
+//   let meh = arr[0]
+  
+//   let currentElement = arr[i] + meh;
+//   if(currentElement < a[i]){
+    
+//   }
+//   if( msf < )
+// }
--- src/app.js
+++ src/app.js
@@ -1,3 +1,2 @@
 require('./Problem1/problem1')
-require('./Problem1/usingQuickSort')
-require('./Problem3/twoPointer')
+require('./MathAlgorithms/fibonacci')
