264bec83fe018159df8c9d713c569374c4ca9de1
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-05-27 13:29:36 +0530

    recent commit

--- src/SortingAlgorithms/insertionSort.js
+++ src/SortingAlgorithms/insertionSort.js
@@ -1,16 +1,16 @@
-function insertin(arr){
-  for(let i =0 ; i < arr.length ; i++){
-     insertElement = arr[i]
-     j = i - 1
-     while( j >=0 && arr[j] > insertElement){
-        console.log("[arrj]:",arr[j])
-        arr[j + 1] = arr[j]
+function insertSort(arr){
+  for(let i =0 ; i<arr.length ; i++){
+   let j = i - 1
+   let numberToinsert = arr[i]
+   while( j >=0 && arr[j]> numberToinsert ){
+    arr[j+1] = arr[j]
+    j = j -1
+   }
+   arr[j+1] = numberToinsert
+  }
+}
 
-     }
-     console.log("arr[j+1]", arr[j + 1], "insertElement", insertElement);
-     arr[j+1] = insertElement
-}
-}
-arr = [4, -2, 6, -4, 7];
-insertin();
-console.log(arr);
+const arr = [-4,5,-6,8,3]
+
+insertSort(arr)
+console.log(arr)
