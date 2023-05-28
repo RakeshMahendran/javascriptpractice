105a18c7181d3ac885f11eac5f5d72813e705ffc
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-05-28 23:45:45 +0530

    variable overshadowing

---
+++ src/javascriptOverShawdowing.js
@@ -0,0 +1,9 @@
+// When a varible declared in a global scope is overshadowed by the variable declared in a block scope, it is called as variabe overshadowing
+
+var a = 5
+function hmm (){
+   a = 2
+   console.log(a)
+}
+
+console.log("a2",a)
