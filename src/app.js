a60aafabc456e4e0524200b61d526230a185f285
rakesh <rakeshmahendran99@gmail.com>
2023-11-01 22:16:26 +0530

    recent commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-// require('./Problem2/customReverse')
-//  //require('./MathAlgorithms/fibonacci')
-// require("./MathAlgorithms/powerOfTwo");
-// require("../src/RecursiveAlgorithms/recursiveFactorial")
-// require("../src/SearchAlgorithms/recursiveBinarySearch")
-// require('../src/SortingAlgorithms/insertionSort')
-// require('../src/javascriptOverShawdowing')
-// require('../src/javascriptOverShawdowing')
-//require('../src/mainOjbect')
-//require('../src/candies')
-  require('../src/LinkedList/positiveArr')
+var removeDuplicates = function(nums) {
+  let n = nums.length - 1
+  
+  let expectedNums = []
+  
+  for (let i=0; i <= n; i++) {
+      cele = nums[i]
+
+      if(!expectedNums.includes(cele)){
+        expectedNums.push(cele)
+      }
+  }
+  return expectedNums
+};
+
+nums = [1,2,3,4,5,5,6,6]
+
+console.log(removeDuplicates(nums))
